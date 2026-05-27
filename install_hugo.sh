38719575d5b6ced5977c8d21177fbc1f5199dba1
魏俊浩 <1206423308@qq.com>
2026-05-27 15:33:58 +0800

    更新构建脚本

--- install_hugo.sh
+++ install_hugo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.158.0/hugo_extended_0.158.0_linux-amd64.tar.gz -o hugo.tar.gz
-tar -xzf hugo.tar.gz
-chmod +x hugo
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.158.0/hugo_extended_0.158.0_linux-amd64.tar.gz -o /tmp/hugo.tar.gz
+tar -xzf /tmp/hugo.tar.gz -C /tmp
+cp /tmp/hugo /usr/local/bin/hugo
