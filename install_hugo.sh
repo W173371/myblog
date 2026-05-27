226aff598eaa32a80070f408fbeacac6ac10acb8
魏俊浩 <1206423308@qq.com>
2026-05-27 15:37:21 +0800

    改用 npm 安装 Hugo

--- install_hugo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.158.0/hugo_extended_0.158.0_linux-amd64.tar.gz -o /tmp/hugo.tar.gz
-tar -xzf /tmp/hugo.tar.gz -C /tmp
-cp /tmp/hugo /usr/local/bin/hugo
