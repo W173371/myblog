20b4279d9f3814ef8eb2c26d988344d6f5dc75eb
魏俊浩 <1206423308@qq.com>
2026-05-27 15:55:18 +0800

    直接下载官方 Dart Sass 二进制

---
+++ get-sass.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+curl -fsSL https://github.com/sass/dart-sass/releases/download/1.86.0/dart-sass-1.86.0-linux-x64.tar.gz -o /tmp/dart-sass.tar.gz
+tar -xzf /tmp/dart-sass.tar.gz -C /tmp
+cp /tmp/dart-sass/sass ./node_modules/.bin/sass
+chmod +x ./node_modules/.bin/sass
+echo "Dart Sass installed: $(./node_modules/.bin/sass --version)"
