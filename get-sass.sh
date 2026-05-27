87a545e39361963c4324ebc8c4f279162680231c
魏俊浩 <1206423308@qq.com>
2026-05-27 16:00:15 +0800

    修复 Dart Sass 路径到系统目录

--- get-sass.sh
+++ get-sass.sh
@@ -2,6 +2,6 @@
 set -e
 curl -fsSL https://github.com/sass/dart-sass/releases/download/1.86.0/dart-sass-1.86.0-linux-x64.tar.gz -o /tmp/dart-sass.tar.gz
 tar -xzf /tmp/dart-sass.tar.gz -C /tmp
-cp /tmp/dart-sass/sass ./node_modules/.bin/sass
-chmod +x ./node_modules/.bin/sass
-echo "Dart Sass installed: $(./node_modules/.bin/sass --version)"
+cp /tmp/dart-sass/sass /usr/local/bin/sass
+chmod +x /usr/local/bin/sass
+echo "Dart Sass installed: $(sass --version)"
